6234745baa1efa82103baf1468051477fd7231aa
xiaodu <xiaodu.li@tuya.com>
2021-12-13 20:08:16 +0800

    slice学习

--- src/slice.go
+++ src/slice.go
@@ -3,9 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("开始了")
-	//今天就开始慢慢计划
-
 	var array [10]int
 
 	var slice = array[5:6]
@@ -13,4 +10,15 @@ func main() {
 	fmt.Println("lenth of slice: ", len(slice))
 	fmt.Println("capacity of slice: ", cap(slice))
 	fmt.Println(&slice[0] == &array[5])
+
+	var slice2 []int
+	slice2 = append(slice2, 1, 2, 3)
+
+	newSlice := AddElement(slice2, 4)
+	fmt.Println(&slice2[0] == &newSlice[0])
+
+}
+
+func AddElement(slice []int, e int) []int {
+	return append(slice, e)
 }
